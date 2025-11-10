4b427051b818a0cb7e02f22f477a5ecc297ae334
Michele Pangrazzi <xmikex83@gmail.com>
2025-11-10 11:08:28 +0100

    Update same pipeline using Chat Generators (#4)
    * using OFFLINE_MODE to avoid both user with updates
    
    * Update sample pipeline using ChatPromptBuilder and OpenAIChatGenerator
    
    * Update pipelines/chat_with_website_streaming/pipeline_wrapper.py
    
    Co-authored-by: Sebastian Husch Lee <10526848+sjrl@users.noreply.github.com>
    
    * Update pipelines/chat_with_website_streaming/pipeline_wrapper.py
    
    Co-authored-by: Sebastian Husch Lee <10526848+sjrl@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Sebastian Husch Lee <10526848+sjrl@users.noreply.github.com>

--- pipelines/chat_with_website_streaming/pipeline_wrapper.py
+++ pipelines/chat_with_website_streaming/pipeline_wrapper.py
@@ -1,12 +1,18 @@
 from pathlib import Path
 from typing import Generator, List, Union
+from hayhooks import streaming_generator
 from haystack import Pipeline
-from hayhooks.server.pipelines.utils import get_last_user_message, streaming_generator
+from hayhooks.server.pipelines.utils import get_last_user_message
 from hayhooks.server.utils.base_pipeline_wrapper import BasePipelineWrapper
 from hayhooks.server.logger import log
 
 
-URLS = ["https://haystack.deepset.ai", "https://www.redis.io", "https://ssi.inc"]
+URLS = [
+    "https://haystack.deepset.ai",
+    "https://www.redis.io",
+    "https://ssi.inc",
+    "https://www.deepset.ai",
+]
 
 
 class PipelineWrapper(BasePipelineWrapper):
@@ -15,12 +21,29 @@ class PipelineWrapper(BasePipelineWrapper):
         self.pipeline = Pipeline.loads(pipeline_yaml)
 
     def run_api(self, urls: List[str], question: str) -> str:
-        log.trace(f"Running pipeline with urls: {urls} and question: {question}")
-        result = self.pipeline.run({"fetcher": {"urls": urls}, "prompt": {"query": question}})
-        return result["llm"]["replies"][0]
+        """
+        Ask a question to a list of websites.
 
-    def run_chat_completion(self, model: str, messages: List[dict], body: dict) -> Union[str, Generator]:
-        log.trace(f"Running pipeline with model: {model}, messages: {messages}, body: {body}")
+        Args:
+            urls (List[str]): List of URLs to ask the question to.
+            question (str): Question to ask the websites.
+
+        Returns:
+            str: Answer to the question.
+        """
+        result = self.pipeline.run(
+            {"fetcher": {"urls": urls}, "prompt": {"query": question}}
+        )
+
+        # result["llm"]["replies"][0] is a ChatMessage instance
+        return result["llm"]["replies"][0].text
+
+    def run_chat_completion(
+        self, model: str, messages: list[dict], body: dict
+    ) -> Union[str, Generator]:
+        log.trace(
+            f"Running pipeline with model: {model}, messages: {messages}, body: {body}"
+        )
 
         question = get_last_user_message(messages)
         log.trace(f"Question: {question}")
@@ -28,5 +51,8 @@ class PipelineWrapper(BasePipelineWrapper):
         # Streaming pipeline run, will return a generator
         return streaming_generator(
             pipeline=self.pipeline,
-            pipeline_run_args={"fetcher": {"urls": URLS}, "prompt": {"query": question}},
+            pipeline_run_args={
+                "fetcher": {"urls": URLS},
+                "prompt": {"query": question},
+            },
         )
